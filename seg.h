a6f46c9b13b099e54c257e2bd9d0ea05a5ff1d09
dormz <564040857@qq.com>
2023-11-29 10:02:59 +0800

    add seg record

---
+++ seg.h
@@ -0,0 +1,18 @@
+//
+// Created by dormz on 2023/11/26.
+//
+
+#ifndef QTREADELF_SEG_H
+#define QTREADELF_SEG_H
+
+/**
+ *
+ *
+ * .rodata
+ * .rodata 段是指只读数据段（read-only data segment），通常用于存储程序中的只读数据，例如常量、字符串字面量等。
+ * 这些数据在程序运行过程中不可被修改，因此放置在只读数据段中有助于提高程序的安全性和稳定性。
+ *
+ */
+
+
+#endif //QTREADELF_SEG_H
